e40a0a99dc661c99611be447b0322a21f7cac70b
winston.shih88 <winston.shih88@gmail.com>
2020-05-19 21:55:29 -0700

    adding module work

---
+++ js/app.js
@@ -0,0 +1,23 @@
+// import the data from data.js
+const tableData = data;
+// Reference the HTML table using d3
+var tbody = d3.select("tbody");
+
+function buildTable(data){
+    // Clearing out any pre-existing data.
+    tbody.html("");
+    // Looping through each object in the data.
+    // Append a row and cells for each value in the row.
+    data.forEach((dataRow) => {
+        // Append a row to the table body.
+        let row = tbody.append("tr");
+
+        // Loop thru each field in the dataRow
+        // Adding each value as a table cell (td)
+        Object.values(dataRow).forEach((val) => {
+            let cell = row.append("td");
+            cell.text(val);
+        });
+    });
+}
+
